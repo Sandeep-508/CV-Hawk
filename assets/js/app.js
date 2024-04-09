4b686d7446f085a91bfd7d6da2e509bbdf538a95
Sandeep-508 <elsenothing321@gmail.com>
2024-04-09 15:09:30 +0530

    made the accordion in the faq section

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,30 +4,34 @@ let body = document.body;
 let first_line = document.querySelector(".first_line")
 let second_line = document.querySelector(".second_line")
 let third_line = document.querySelector(".third_line")
+let nav_link = document.querySelectorAll(".nav_link");
 function cross() {
     second_line.style.display = "none";
-    // first_line.classList.add("transform-origin-center-center");
     first_line.style.transform = "rotate(45deg)";
     first_line.style.top = "13px";
     first_line.style.left = "2px";
     first_line.classList.add("position-relative");
-    // third_line.classList.add("transform-origin-center-center");
     third_line.style.transform = "rotate(-45deg)";
     third_line.style.top = "-12px";
     third_line.classList.add("position-relative");
 }
 function remove_cross() {
     second_line.style.display = "block";
-    // first_line.classList.add("transform-origin-center-center");
     first_line.style.transform = "rotate(0deg)";
     first_line.classList.remove("position-relative");
-    // third_line.classList.add("transform-origin-center-center");
     third_line.style.transform = "rotate(0deg)";
     third_line.classList.remove("position-relative");
 }
 
 menuicon.addEventListener("click", () => {
     lg_view.classList.toggle("show");
+    nav_link.forEach(other => {
+        other.addEventListener("click", () => {
+            lg_view.classList.remove("show");
+            body.style.overflow = "auto";
+            remove_cross();
+        });
+    });
     if (lg_view.classList.contains("show")) {
         body.style.overflow = "hidden";
         cross();
@@ -35,4 +39,38 @@ menuicon.addEventListener("click", () => {
         body.style.overflow = "auto";
         remove_cross();
     }
+});
+
+
+
+// accoridon
+
+let accordion_item = document.querySelectorAll(".accoridon_items");
+
+accordion_item.forEach(otherElement => {
+    let svg_icon = otherElement.querySelector(".svg_icon");
+    let disp_text = otherElement.querySelector(".disp_text");
+    let clickBtn = otherElement.querySelector(".click_btn");
+    clickBtn.addEventListener("click", () => {
+        accordion_item.forEach(itm => {
+            if (itm !== otherElement) {
+                let svg_icon = itm.querySelector(".svg_icon");
+                let disp_text = itm.querySelector(".disp_text");
+                disp_text.style.display = "none";
+                svg_icon.style.transform = "rotate(0deg)";
+                itm.classList.remove("disp_change");
+            }
+        });
+        let text_prop = window.getComputedStyle(disp_text).display;
+        if (text_prop === "none") {
+            disp_text.style.display = "flex";
+            svg_icon.style.transform = "rotate(180deg)";
+            otherElement.classList.add("disp_change");  
+        } else {
+            disp_text.style.display = "none";
+            svg_icon.style.transform = "rotate(0deg)";
+            otherElement.classList.remove("disp_change");
+        }
+    });
+
 });
