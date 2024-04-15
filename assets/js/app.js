24415ec3d95cc145778b0f748db53088443892cf
Sandeep-508 <elsenothing321@gmail.com>
2024-04-15 12:28:39 +0530

    added progress bar in the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -157,3 +157,15 @@ AOS.init({
     once: true,
     duration: 1200,
 });
+
+
+// progress
+
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
